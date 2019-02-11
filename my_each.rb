b3ec74af3aba3d7526b035ceaab1ad44411f716b
Mark Pothecary <markpothers@hotmail.com>
2019-02-11 02:27:19 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+
+
+    x = 0
+    while x < array.length
+      yield array[x]
+      x += 1
+    end
+
+
+    array
+  else
+    print "This block should not run!"
+  end
 end
