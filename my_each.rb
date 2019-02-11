b1ea6ce2456ffadf1aa49b46342d0307dc35c0b9
Mark Pothecary <markpothers@hotmail.com>
2019-02-11 02:21:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,17 @@
 def my_each(array)
+  if block.given?
 
-  x = 0
-  while x < array.length
-    print array[x]
-    x += 1
+
+    x = 0
+    while x < array.length
+      yield array[i]
+      puts array[x]
+      x += 1
+    end
+
+
+    array
+  else
+    puts "This block should not run!"
   end
-
-
-
-
-
-
 end
